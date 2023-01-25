1eabfde7b753900c711e70f1fd8fcaa7b79d6544
Alex Markoskyi <bnc@ukr.net>
2023-01-25 20:36:55 +0200

    Modify: 1. core .gitignore (adding new folder) 2. modify main.py (adding new applications) 3. modify paths.py (adding new volume for path to file) and small modify in other files.

--- application/get_csv_from_googledrive/request.py
+++ application/get_csv_from_googledrive/request.py
@@ -1,8 +1,21 @@
-import pandas as pd
-
+import requests
 from application.config.paths import FILES_INPUT_PATH
+from application.logging.loggers import get_core_logger
+from application.get_csv_from_googledrive.data_processing import calculate_data
 
-df = pd.read_csv(f"{FILES_INPUT_PATH}/people_data(extended).csv")
-pd.set_option("display.max_columns", None)
 
-print(df["Index"].columns)
+def requests__file_from_googledrive():
+    logger = get_core_logger()
+    url = "https://drive.google.com/uc?export=download&id=13nk_FYpcayUck2Ctrela5Tjt9JQbjznt"
+
+    path_to_file = FILES_INPUT_PATH.joinpath("download_file.csv")
+
+    with requests.Session() as session:
+        response = session.get(url)
+        logger.info(f"{response=}")
+
+        csv_file = open(path_to_file, "wb")
+        csv_file.write(response.content)
+        csv_file.close()
+
+    logger.info(f"\nPath to file: file://{path_to_file}\nDONE.\n\n" f"Answer is.\n{calculate_data()}")
--- main.py
+++ main.py
@@ -4,7 +4,9 @@
 # from application.requests_example.requests_example import requests_example
 
 from application.astronauts.data_processing import astronauts_names
+from application.get_csv_from_googledrive.request import requests__file_from_googledrive
 from application.logging.init_logging import init_logging
+from application.simple_co_file.write_open_file import create_read_file
 from application.users_generator.users_file_example_txt import users_file_example_txt
 
 
@@ -17,8 +19,17 @@ def main() -> None:
     # requests_example()
     # ---------------------------------------
 
+    # Simple create file in folder "special_folder" and open file with some text
+    create_read_file()
+
+    # Generate users
     users_file_example_txt()
+
+    # Astronauts names and quality
     astronauts_names()
+
+    # Download *.csv file from Google Drive and processing data
+    requests__file_from_googledrive()
 
 
 if __name__ == "__main__":
